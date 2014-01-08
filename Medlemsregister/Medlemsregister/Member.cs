08e581cbb01c8e3e15541713c75d9279f55577be
MarkusGirdland <mg222pi@student.lnu.se>
2014-01-08 11:14:31 +0100

    Meny & Början av Class

---
+++ Medlemsregister/Medlemsregister/Member.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medlemsregister
+{
+    class Member
+    {
+        private string _firstName;
+        private string _lastName;
+        private int _phoneNumber;
+        private int _memberNumber;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                foreach (char c in value)
+                {
+                    if (!char.IsLetter(c))
+                        throw new ArgumentException();
+                }
+
+                _firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                foreach (char c in value)
+                {
+                    if (!char.IsLetter(c))
+                        throw new ArgumentException();
+                }
+
+                _lastName = value;
+            }
+        }
+
+        public int PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException();
+                }
+
+                _phoneNumber = value;
+            }
+        }
+
+        public void CreateMemberNumber()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(00000, 99999);
+
+            this._memberNumber = randomNumber;
+        }
+
+        public string ToString()
+        {
+            string returnString;
+
+            returnString = String.Format("\n\nNamn  :\t{0}\nEfternamn   :\t{1}\nTelefonnummer   :\t{2}\nMedlemsnummer   :\t{3}\n", _firstName, _lastName, _phoneNumber, _memberNumber);
+
+            return returnString;
+        }
+
+    }
+}
--- Medlemsregister/Medlemsregister/Program.cs
+++ Medlemsregister/Medlemsregister/Program.cs
@@ -10,6 +10,100 @@ namespace Medlemsregister
     {
         static void Main(string[] args)
         {
+            int input = 0;
+
+            do
+            {
+                ViewMenu();
+
+                try
+                {
+                    input = int.Parse(Console.ReadLine());
+
+                    if (input >= 0 && input <= 5)
+                    {
+                    }
+
+                    else
+                    {
+                        ViewMessage("FEL! Ange ett nummer mellan 0 och 5.", true);
+                    }
+                }
+
+                catch
+                {
+                    ViewMessage("FEL! Ange ett nummer mellan 0 och 5.", true);
+                }
+
+                switch (input)
+                {
+                    case 0:
+                        continue;
+
+                    case 1:
+                        // Skapa medlem
+                        ViewMessage("Ettan", false);
+                        break;
+
+                    case 2:
+                        // Lista medlem
+                        ViewMessage("Tvåan", false);
+                        break;
+
+                    case 3:
+                        //Info om medlem
+                        ViewMessage("Trean", false);
+                        break;
+
+                    case 4:
+                        // Redigera medlem
+                        ViewMessage("Fyran", false);
+                        break;
+
+                    case 5:
+                        // Ta bort medlem
+                        ViewMessage("Femman", false);
+                        break;
+                }
+            } while (input != 0);
         }
+
+
+        static void ViewMenu()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("======================================");
+            Console.WriteLine("=                                    =");
+            Console.WriteLine("=         Medlemsregister            =");
+            Console.WriteLine("=                                    =");
+            Console.WriteLine("======================================\n");
+            Console.ResetColor();
+
+            Console.WriteLine("0. Avsluta.\n");
+            Console.WriteLine("1. Registrera ny medlem \n");
+            Console.WriteLine("2. Lista medlemmar \n");
+            Console.WriteLine("3. Få ut information om medlem \n");
+            Console.WriteLine("4. Redigera medlem \n");
+            Console.WriteLine("5. Ta bort medlem \n");
+            Console.WriteLine("=======================================\n");
+            Console.WriteLine("\nAnge menyval [0-5]:");
+        }
+
+        static void ViewMessage(string message, bool error)
+        {
+            if(error)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+            }
+
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+            }
+
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
     }
 }
