c56c5744b6b66620363ae9c9300d98d28b99b6b1
MarkusGirdland <mg222pi@student.lnu.se>
2014-01-09 11:52:22 +0100

    Lade till whitebox test

---
+++ Medlemsregister/WhiteBox Project/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WhiteBox_Project
+{
+    [TestClass]
+    public class WhiteBox
+    {
+        [TestMethod]
+        public void MemberTest()
+        {
+            
+        }
+    }
+}
